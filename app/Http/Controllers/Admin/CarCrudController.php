6e7aa8bc90cf3a25029addae7b53515a756cd34d
Lina Lazarova <78424163+linalazarova@users.noreply.github.com>
2021-02-12 19:18:01 +0200

    Added an option to upload an image to Cars

--- app/Http/Controllers/Admin/CarCrudController.php
+++ app/Http/Controllers/Admin/CarCrudController.php
@@ -19,6 +19,38 @@ class CarCrudController extends CrudController
     use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
 
+    private function getFieldsData($show = FALSE) {
+        return [
+            [
+                'label' => "Car Image",
+                'name' => "image",
+                'type' => 'image',
+                'crop' => true, // set to true to allow cropping, false to disable
+                'aspect_ratio' => 1, // omit or set to 0 to allow any aspect ratio
+            ],
+            [
+                'name'=> 'car',
+                'label' => 'Car model',
+                'type'=> 'text'
+            ],
+            [
+                'name' => 'brand',
+                'label' => 'Brand',
+                'type' => 'text',
+            ],
+            [
+                'name' => 'year',
+                'label' => 'Year of production',
+                'type' => 'text',
+            ],
+            [
+                'name' => 'price',
+                'label' => 'Price',
+                'type' => 'text',
+            ]
+
+        ];
+    }
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
      * 
@@ -29,6 +61,8 @@ class CarCrudController extends CrudController
         CRUD::setModel(\App\Models\Car::class);
         CRUD::setRoute(config('backpack.base.route_prefix') . '/car');
         CRUD::setEntityNameStrings('car', 'cars');
+
+        $this->crud->addFields($this->getFieldsData());
     }
 
     /**
@@ -39,13 +73,8 @@ class CarCrudController extends CrudController
      */
     protected function setupListOperation()
     {
-        CRUD::setFromDb(); // columns
-
-        /**
-         * Columns can be defined using the fluent syntax or array syntax:
-         * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
-         */
+        $this->crud->set('show.setFromDb', false);
+        $this->crud->addColumns($this->getFieldsData(TRUE));
     }
 
     /**
