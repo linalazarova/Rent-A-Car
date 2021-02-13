6de2f33224f85e12ba03676fc67a4c66c4796b8b
Lina Lazarova <78424163+linalazarova@users.noreply.github.com>
2021-02-13 18:15:28 +0200

    Edited the template according to the theme of the project. Added Database Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,101 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        //Seeding the database
+        \DB::table('brand')->insert([
+            [
+                'id' => 1,
+                'name' => 'Tesla',
+                'year_of_creation' => 2003
+            ],
+            [
+                'id' => 2,
+                'name' => 'Renault',
+                'year_of_creation' => 1899
+            ],
+            [
+                'id' => 3,
+                'name' => 'Mercedes',
+                'year_of_creation' => 1926
+            ]
+        ]);
+
+        \DB::table('carmodels')->insert([
+            [
+                'id' => 1,
+                'name' => 'Model S'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Megane'
+            ],
+            [
+                'id' => 3,
+                'name' => 'X166'
+            ]
+        ]);
+
+        \DB::table('cars')->insert([
+            [
+                'id' => 1,
+                'image' => 'storage/cars/88bab9397c23d9b1b7c35733473db3e0.jpg',
+                'car_model_id' => 1,
+                'brand_id'=> 1,
+                'year_of_production' => 2017               
+            ],
+            [
+                'id' => 2,
+                'image' => 'storage/cars/040467913f4b0a52f35b624b044cf583.jpg',
+                'car_model_id' => 2,
+                'brand_id'=> 2,
+                'year_of_production' => 2004
+            ],
+            [
+                'id' => 3,
+                'image' => 'storage/cars/a25b4729ab3bdbee4e3f97b0af66894b.jpg',
+                'car_model_id' => 3,
+                'brand_id'=> 3,
+                'year_of_production' => 2019
+            ]
+        ]);
+
+        \DB::table('reservations')->insert([
+            [
+                'id' => 1,
+                'first_name'=>"John",
+                'last_name'=>"Doe",
+                'telephpne'=>"0999",
+                'brand_id'=> 1,
+                'car_model_id' => 1,
+                'date_from'=>date('Y-m-d'),
+                'date_to'=>date('Y-m-d'),
+                'price'=>100            
+            ],
+            [
+                'id' => 2,
+                'first_name'=>"Jane",
+                'last_name'=>"Doe",
+                'telephpne'=>"088",
+                'brand_id'=> 2,
+                'car_model_id' => 2,
+                'date_from'=>date('Y-m-d'),
+                'date_to'=>date('Y-m-d'),
+                'price'=>80           
+            ],
+            [
+                'id' => 3,
+                'first_name'=>"Maria",
+                'last_name'=>"Lee",
+                'telephpne'=>"0777",
+                'brand_id'=> 3,
+                'car_model_id' => 3,
+                'date_from'=>date('Y-m-d'),
+                'date_to'=>date('Y-m-d'),
+                'price'=>150           
+            ],
+
+        ]);
     }
 }
+
--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -6,7 +6,55 @@
             <header>
                 <h2>{{$title}}</h2>
             </header>
-            {!! $text !!}
+            <div class="row">
+					<section>
+						<a href="#" class="image"><img src="images/tesla.jpg" alt="" style="width:500px;height:300px;"></a>
+						<header>
+							<h2>Tesla Model S</h2>
+						</header>
+						<ol>
+                        <ul>
+                            <li><b>Brand</b>: Tesla </li>
+                            <li><b>Model</b>: Model S </li>
+                            <li><b>Body type</b>: Liftback sedan </li>
+                            <li><b>Seats</b>: 5</li>
+                            <li><b>Doors</b>: 4</li>
+                            <li><b>Fuel consumption</b>: Electric</li>
+                        </ul>
+                        </ol>
+					</section>
+
+					<section>
+						<a href="#" class="image"><img src="images/renault.jpg" alt="" style="width:400px;height:300px;"></a>
+						<header>
+							<h2>Renault Megane</h2>
+						</header>
+						<ul>
+                            <li><b>Brand</b>: Renault </li>
+                            <li><b>Model</b>: Megane </li>
+                            <li><b>Body type</b>: Cabriolet, Coupe </li>
+                            <li><b>Seats</b>: 5</li>
+                            <li><b>Doors</b>: 4</li>
+                            <li><b>Fuel consumption</b>: 5 l/100 km</li>
+                        </ul>
+					</section>	
+
+                    <section>
+						<a href="#" class="image"><img src="images/mercedes.png" alt="" style="width:400px;height:300px;"></a>
+						<header>
+							<h2>Mercedes GL-Class (X166)</h2>
+						</header>
+						<ul>
+                            <li><b>Brand</b>: Mercedes </li>
+                            <li><b>Model</b>: GL-Class(X166) </li>
+                            <li><b>Body type</b>: SUV </li>
+                            <li><b>Seats</b>: 5</li>
+                            <li><b>Doors</b>: 4</li>
+                            <li><b>Fuel consumption</b>: 12.4 l/100 km</li>
+                        </ul>
+					</section>		
+
+				</div>
         </div>
     </div>
 @endsection
